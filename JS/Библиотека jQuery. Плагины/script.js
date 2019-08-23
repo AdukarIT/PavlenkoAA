d859d3de687a107ac40bc06501a2a4c7774cb30c
Pavlenko Aleksandr <pavlenuch@yandex.ru>
2019-08-23 21:11:42 +0300

    add jquery plagins

--- JS/Библиотека jQuery. Плагины/script.js
+++ JS/Библиотека jQuery. Плагины/script.js
@@ -9,38 +9,137 @@
 // Комментарий к заказу
 // Чекбокс "Я прочитал и согласен с условиями доставки" (обязательное поле)
 // Кнопка "Подтвердить заказ"
-$('#commentForm').validate({
-	rules: {
+// $('#commentForm').validate({
+// 	rules: {
+//     name: {
+//       minlength: 3
+//     },
+//     phone: {
+//       digits: true
+//     },
+//     email: {
+//       email: true
+//     },
+//     delivery: {
+//       required: true
+//     }
+//   },
+//   messages: {
+//     name: {
+//       required: "Введите имя, под которым вы зарегистрированы"
+//     },
+//     phone: {
+//       required: "Введите номер телефона после кода страны",
+//       digits: "Вводите только цифры"
+//     },
+//     email: {
+//       required: "Введите ваш email",
+//       email: "Введите в формате name@name.com"
+//     },
+//     delivery: {
+//       required: "Выберите способ доставки"
+//     },
+//     agree: {
+//       required: "Подтвердите свое согласие"
+//     }
+//   }
+// });
+
+// домашняя работа
+
+// красивая дата
+$( "#datepicker" ).datepicker();
+
+// добавляем selectable
+$( ".ui-widget>tbody" ).selectable({filter: 'tr'});
+
+// диалоговое окно для удаления строки
+$( function() {
+  $( "#dialog-confirm" ).dialog({
+    autoOpen: false
+  });
+  $( "#delete-comment" ).button().on( "click", function() {
+  $( "#dialog-confirm" ).dialog( "open" );
+});
+  $( "#dialog-confirm" ).dialog({
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      "Delete all items": function() {
+        $( this ).dialog( "close" );
+        $('#users').find(".ui-selected", this).remove();
+      },
+      Cancel: function() {
+        $( this ).dialog( "close" );
+      }
+    }
+  });
+});
+
+$('#new_comment').validate({
+  rules: {
     name: {
       minlength: 3
     },
-    phone: {
+    tel: {
       digits: true
     },
-    email: {
-      email: true
-    },
-    delivery: {
-      required: true
+    comment:{
+      minlength: 6
     }
   },
   messages: {
     name: {
-      required: "Введите имя, под которым вы зарегистрированы"
+      required: "Введите ваше имя"
     },
-    phone: {
+    tel: {
       required: "Введите номер телефона после кода страны",
       digits: "Вводите только цифры"
     },
-    email: {
-      required: "Введите ваш email",
-      email: "Введите в формате name@name.com"
+    datepicker: {
+      required: "Введите ваш email"
     },
-    delivery: {
-      required: "Выберите способ доставки"
-    },
-    agree: {
-      required: "Подтвердите свое согласие"
+    comment: {
+      required: "Введите тип обращения"
     }
   }
+});
+
+function addUser() {
+  $('#new_comment').submit();
+    $( "#users tbody" ).append( "<tr>" +
+    "<td>" + $( "#name" ).val() + "</td>" +
+    "<td>" + $( "#tel" ).val() + "</td>" +
+    "<td>" + $( "#datepicker" ).val() + "</td>" +
+    "<td>" + $( "#comment" ).val() + "</td>" +
+    "</tr>" );
+    dialog.dialog( "close" );
+}
+
+
+let dialog = $( "#form_comment" ).dialog({
+      autoOpen: false,
+      height: 425,
+      width: 350,
+      modal: true,
+      buttons: {
+        "Сохранить": addUser,
+        "Отмена": function() {
+          dialog.dialog( "close" );
+        }
+      },
+      close: function() {
+        form[ 0 ].reset();
+      }
+    });
+
+let form = dialog.find( "form" ).on( "submit", function( event ) {
+  event.preventDefault();
+  addUser();
+});
+
+$( "#add-comment" ).button().on( "click", function() {
+  dialog.dialog( "open" );
 });
